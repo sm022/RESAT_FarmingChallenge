7c5668cd3b8abd912710003747f455356b2bb1bb
sm <sungminnim97@gmail.com>
2023-06-27 12:43:08 +0900

    코드 리팩토링

--- calendar.js
+++ calendar.js
@@ -1,59 +1,58 @@
-      document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
+  // Select all elements with the class "has-memo" and add the class to them
+  const memoCells = document.querySelectorAll('.calendar-table td.has-memo');
+  memoCells.forEach(cell => {
+    cell.classList.add('has-memo');
+  });
 
-        // "has-memo" 클래스를 가진 모든 요소를 선택하고 클래스 추가
-      const memoCells = document.querySelectorAll('.calendar-table td.has-memo');
-      memoCells.forEach(cell => {
-        cell.classList.add('has-memo');
-      });
-        // 캘린더 상수
-        const DAYS_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
-        const MONTHS_OF_YEAR = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
+  // Calendar Constants
+  const DAYS_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
+  const MONTHS_OF_YEAR = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
 
-        // 현재 날짜 가져오기
-        const currentDate = new Date();
+  // Get current date
+  const currentDate = new Date();
 
-        // 캘린더 상태
-        let selectedDate = null;
-        let memoData = {};
-        let editMode = false; // Add editMode variable and set initial value to false
+  // Calendar State
+  let selectedDate = null;
+  let memoData = {};
+  let editMode = false; // Add editMode variable and set initial value to false
 
+  // Load memo data from local storage
+  const storedMemoData = localStorage.getItem('memoData');
+  if (storedMemoData) {
+    memoData = JSON.parse(storedMemoData);
+  }
 
-        // 로컬 스토리지에서 메모 데이터 불러오기 
-        const storedMemoData = localStorage.getItem('memoData');
-        if (storedMemoData) {
-          memoData = JSON.parse(storedMemoData);
-        }
+  // DOM Elements
+  const currentMonthElement = document.querySelector('.current-month');
+  const calendarTableBody = document.querySelector('.calendar-table tbody');
+  const prevMonthButton = document.querySelector('.prev-month');
+  const nextMonthButton = document.querySelector('.next-month');
+  const memoOverlay = document.querySelector('.memo-overlay');
+  const memoDialog = document.querySelector('.memo-dialog');
+  const memoTextarea = document.querySelector('.memo-textarea');
+  const editMemoButton = document.querySelector('.edit-memo');
+  const saveMemoButton = document.querySelector('.save-memo');
+  const closeDialogButton = document.querySelector('.close-dialog');
 
-        // DOM 요소
-        const currentMonthElement = document.querySelector('.current-month');
-        const calendarTableBody = document.querySelector('.calendar-table tbody');
-        const prevMonthButton = document.querySelector('.prev-month');
-        const nextMonthButton = document.querySelector('.next-month');
-        const memoOverlay = document.querySelector('.memo-overlay');
-        const memoDialog = document.querySelector('.memo-dialog');
-        const memoTextarea = document.querySelector('.memo-textarea');
-        const editMemoButton = document.querySelector('.edit-memo');
-        const saveMemoButton = document.querySelector('.save-memo');
-        const closeDialogButton = document.querySelector('.close-dialog');
+  // Render Calendar
+  function renderCalendar() {
+    // Clear calendar
+    calendarTableBody.innerHTML = '';
 
-        // 캘린더 렌더링
-    function renderCalendar() {
-      // 캘린더 초기화 
-      calendarTableBody.innerHTML = '';
+    // Get current month details
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayOfWeek = new Date(currentYear, currentMonth, 1).getDay();
 
-      // 현재 Month 세부 정보 가져오기 
-      const currentYear = currentDate.getFullYear();
-      const currentMonth = currentDate.getMonth();
-      const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-      const firstDayOfWeek = new Date(currentYear, currentMonth, 1).getDay();
+    // Set current month text
+    currentMonthElement.textContent = currentYear + '년 ' + MONTHS_OF_YEAR[currentMonth];
 
-      // 현재 Month text 설정
-      currentMonthElement.textContent = currentYear + '년 ' + MONTHS_OF_YEAR[currentMonth];
-
-      // 캘린더 행과 셀 생성 
-      let date = 1;
-      for (let i = 0; i < 6; i++) {
-        const row = document.createElement('tr');
+    // Create calendar rows and cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+      const row = document.createElement('tr');
 
       for (let j = 0; j < 7; j++) {
         if (i === 0 && j < firstDayOfWeek) {
@@ -72,19 +71,19 @@
             cell.classList.add('selected');
           }
 
-            const dateKey = cell.dataset.date;
-            if (memoData[dateKey]) {
-              cell.classList.add('has-memo'); // 메모가 있는 셀에 .has-memo 클래스 추가
-              const memoIndicator = document.createElement('div');
-              memoIndicator.classList.add('memo-indicator');
-              cell.appendChild(memoIndicator);
-            }
+          const dateKey = cell.dataset.date;
+          if (memoData[dateKey]) {
+            cell.classList.add('has-memo'); // Add .has-memo class to cells with memo
+            const memoIndicator = document.createElement('div');
+            memoIndicator.classList.add('memo-indicator');
+            cell.appendChild(memoIndicator);
+          }
 
-            // 메모 click 이벤트리스너 추가
-            cell.addEventListener('click', function(event) {
-              event.stopPropagation(); // 클릭 이벤트가 부모 요소로 전파 방지
-              selectDate(cell); // 클릭된 셀과 함께 selectDate 함수를 호출
-            });
+          // Add memo click event listener
+          cell.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevent click event from propagating to parent elements
+            selectDate(cell); // Call selectDate function with the clicked cell
+          });
 
           row.appendChild(cell);
           date++;
@@ -95,96 +94,93 @@
     }
   }
 
+  // Select Date
+  function selectDate(dayElement) {
+    const day = parseInt(dayElement.textContent);
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    selectedDate = new Date(currentYear, currentMonth, day);
+    renderCalendar();
 
-
-    // 폼 없이 메모 보기
-function showMemo() {
-  if (selectedDate) {
-    const dateString = selectedDate.toISOString().split('T')[0];
-    const memoContent = memoData[dateString] || '';
-
-    memoOverlay.style.display = 'flex';
-
-    if (memoContent) {
-      // 선택된 날짜에 메모가 있을 경우
-      memoTextarea.value = memoContent;
-      memoTextarea.readOnly = true;
-      saveMemoButton.style.display = 'none'; // 저장 버튼 숨기기
-      editMemoButton.style.display = 'inline-block'; // 수정 버튼 보이기
+    if (memoData[selectedDate.toISOString().split('T')[0]]) {
+      editMemo();
     } else {
-      // 선택된 날짜에 메모가 없을 경우
-      memoTextarea.value = '';
-      memoTextarea.readOnly = false;
-      saveMemoButton.style.display = 'inline-block'; // 저장 버튼 숨기기
-      editMemoButton.style.display = 'none'; // 수정 버튼 보이기
+      openMemoDialog();
     }
   }
-}
 
-
-        // 날짜 선택 
-function selectDate(dayElement) {
-  const day = parseInt(dayElement.textContent);
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth();
-
-  selectedDate = new Date(currentYear, currentMonth, day);
-  renderCalendar();
-
-  if (memoData[selectedDate.toISOString().split('T')[0]]) {
-    // 선택된 날짜에 메모가 있는 경우
-    editMode = true; // 수정 모드를 true로 설정
-    openMemoDialog();
-  } else {
-    // 선택된 날짜에 메모가 없는 경우
-    editMode = false; // 수정 모드를 false로 설정
-    openMemoDialog();
+  // Open Memo Dialog
+  function openMemoDialog() {
+    if (selectedDate) {
+      const dateString = selectedDate.toISOString().split('T')[0];
+      const memoContent = memoData[dateString] || '';
+      memoTextarea.value = memoContent;
+      memoTextarea.readOnly = false;
+      saveMemoButton.style.display = 'inline-block';
+      editMemoButton.style.display = 'none';
+      memoOverlay.style.display = 'flex';
+      memoTextarea.focus();
+    }
   }
-}
 
-
-
-        // 메모창 열기
-function openMemoDialog() {
-  if (selectedDate) {
-    const dateString = selectedDate.toISOString().split('T')[0];
-    const memoContent = memoData[dateString] || '';
-
-    memoTextarea.value = memoContent;
-    memoTextarea.readOnly = false;
-    saveMemoButton.style.display = 'inline-block';
-    editMemoButton.style.display = 'none';
-
-    memoOverlay.style.display = 'flex';
-    memoTextarea.focus();
+  // Edit Memo
+  function editMemo() {
+    if (selectedDate) {
+      const dateString = selectedDate.toISOString().split('T')[0];
+      const memoContent = memoData[dateString] || '';
+      memoTextarea.value = memoContent;
+      memoTextarea.readOnly = true;
+      saveMemoButton.style.display = 'inline-block';
+      editMemoButton.style.display = 'none';
+      memoOverlay.style.display = 'flex';
+      memoTextarea.focus();
+    }
   }
-}
 
+  // Save memo
+  function saveMemo() {
+    if (selectedDate) {
+      const date = selectedDate.toISOString().split('T')[0];
+      memoData[date] = memoTextarea.value;
 
+      // Update selected cell with memo indicator
+      const selectedCell = calendarTableBody.querySelector(`[data-date="${date}"]`);
+      if (selectedCell) {
+        const memoIndicator = selectedCell.querySelector('.memo-indicator');
+        if (memoData[date]) {
+          if (!memoIndicator) {
+            const newMemoIndicator = document.createElement('div');
+            newMemoIndicator.classList.add('memo-indicator');
+            selectedCell.appendChild(newMemoIndicator);
+          }
+        } else {
+          if (memoIndicator) {
+            selectedCell.removeChild(memoIndicator);
+          }
+        }
+      }
+    }
 
-      // 메모 수정
-function editMemo() {
-  if (selectedDate) {
-    const dateString = selected창 닫기
-    renderCalendar(); // 메모 indicator 업데이트를 위해 캘린더 다시 렌더링
+    editMode = false;
+    closeMemoDialog(); // Close the dialog after saving the memo
+    renderCalendar(); // Render the calendar to update memo indicators
 
-    // 메모 데이터를 로컬 스토리지에 저장
+    // Save memo data to local storage
     localStorage.setItem('memoData', JSON.stringify(memoData));
   }
 
-      // 메모창 닫기기
-      function closeMemoDialog() {
-        memoOverlay.style.display = 'none';
-        memoTextarea.value = '';
-        selectedDate = null;
-      }
+  // Close Memo Dialog
+  function closeMemoDialog() {
+    memoOverlay.style.display = 'none';
+    memoTextarea.value = '';
+    selectedDate = null;
+  }
 
-
-        // Event Listeners
-        prevMonthButton.addEventListener('click', function() {
-          currentDate.setMonth(currentDate.getMonth() - 1);
-          renderCalendar();
-        });
+  // Event Listeners
+  prevMonthButton.addEventListener('click', function() {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    renderCalendar();
+  });
 
   nextMonthButton.addEventListener('click', function() {
     currentDate.setMonth(currentDate.getMonth() + 1);
@@ -202,7 +198,6 @@ function editMemo() {
   editMemoButton.addEventListener('click', editMemo);
   closeDialogButton.addEventListener('click', closeMemoDialog);
 
-        // 초기 렌더링
-        renderCalendar(); 
-        
-      });
+  // Initial Render
+  renderCalendar();
+});
